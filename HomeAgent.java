9332f0dd169c451cd207d38d739088e150effce2
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-05-09 23:51:15 +0200

    React to attack on base (not tested)

--- HomeAgent.java
+++ HomeAgent.java
@@ -17,8 +17,14 @@ public class HomeAgent extends Agent {
 	 */
 	@Override
 	public void update() {
-		// TODO: detect enemy presence near base => do everything we can to protect
-		
+		// When we detect enemy presence near base
+		// => do everything we can to protect it
+		float danger = Map.evaluateDanger(Map.base);
+		if (danger > 0) {
+			Objective reaction = new OccupyObjective(Map.base);
+			reaction.setSpendingLimit((int)(danger * Interface.COUT_SORCIER * 2));
+			addObjective(reaction);
+		}
 	}
 
 }
